76d554ddcb4e55f994772453ddcd98ed930d3f18
Robin Ryan <robin1ryan@hotmail.com>
2025-03-18 10:17:02 +0100

    removed the animation, too much work to make it animate nicely

--- src/components/DEFAULTPAGE/DefaultPage.jsx
+++ src/components/DEFAULTPAGE/DefaultPage.jsx
@@ -4,28 +4,30 @@ import React from "react";
 
 const PictureSlider = () => {
   const [activeImage, setActiveImage] = useState(1);
-  const [prevImage, setPrevImage] = useState(2);
   const [mouseHoverSlider, setMouseHoverSlider] = useState(false);
 
-  const images = [{ img: "imgOne" }, { img: "imgTwo" }, { img: "imgThree" }];
+  const images = [
+    { img: "imgOne" },
+    { img: "imgTwo" },
+    { img: "imgThree" },
+    { img: "imgFour" },
+    { img: "imgFive" },
+    { img: "imgSix" },
+  ];
 
   const addActiveImage = () => {
     if (activeImage < images.length - 1) {
-      setPrevImage(activeImage);
       setActiveImage((prevImg) => prevImg + 1);
     } else {
       setActiveImage(0);
-      setPrevImage(2);
     }
   };
 
   const subtractActiveImage = () => {
     if (activeImage >= 1) {
-      setPrevImage(activeImage);
       setActiveImage((prevImg) => prevImg - 1);
     } else {
       setActiveImage(images.length - 1);
-      setPrevImage(0);
     }
   };
 
@@ -63,26 +65,9 @@ const PictureSlider = () => {
         >
           {images.map((img, index) => (
             <React.Fragment key={index}>
-              <div
-                key={index}
-                className={classes.pictureSliderImage}
-                style={{
-                  marginLeft:
-                    index === activeImage
-                      ? "0%"
-                      : index === activeImage + 1
-                      ? "100%"
-                      : index === images.length - 1 || index === activeImage - 1
-                      ? "-100%"
-                      : "100%",
-                  transition:
-                    index === prevImage || index === activeImage
-                      ? "all 1s ease"
-                      : "",
-                }}
-              >
-                {index}
-              </div>
+              {index === activeImage && (
+                <div className={classes.pictureSliderImage}>{index}</div>
+              )}
             </React.Fragment>
           ))}
         </div>
