cea41eca015dfc5b03403a4e9a9b56b81ce48f06
Robin Ryan <robin1ryan@hotmail.com>
2025-03-18 16:41:35 +0100

    think im done with the smaller picture-wheel. Damn these things require a lot of logic and thinking

--- src/components/DEFAULTPAGE/DefaultPage.jsx
+++ src/components/DEFAULTPAGE/DefaultPage.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import classes from "./defaultPage.module.scss";
 import React from "react";
 
 const PictureSliderSmall = ({ size }) => {
+  const [activeImage, setActiveImage] = useState(0);
   const images = [];
+
+  const elementTarget = useRef(null);
+  const [elementWidth, setElementWidth] = useState(null);
 
   const createImages = () => {
     for (let i = 0; i < size; i++) {
@@ -11,12 +15,69 @@ const PictureSliderSmall = ({ size }) => {
     }
   };
   createImages();
+
+  const addActiveImage = () => {
+    if (activeImage < 9) {
+      setActiveImage((prevImg) => prevImg + 1);
+    }
+  };
+
+  const subtractActiveImage = () => {
+    if (activeImage >= 1) {
+      setActiveImage((prevImg) => prevImg - 1);
+    }
+  };
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => {
+      if (elementTarget.current) {
+        setElementWidth(elementTarget.current.offsetWidth);
+      }
+    });
+
+    if (elementTarget.current) {
+      resizeObserver.observe(elementTarget.current);
+    }
+
+    return () => {
+      if (elementTarget.current) {
+        resizeObserver.unobserve(elementTarget.current);
+      }
+    };
+  }, []);
+
+  console.log(elementWidth * activeImage);
+
+  console.log(elementWidth);
+
   return (
     <div className={classes.smallPictureSlider}>
       <div className={classes.pictureSliderContainer}>
-        {images.map((img, index) => (
-          <div key={index} className={classes.pictureSliderImage}></div>
-        ))}
+        <button
+          onClick={subtractActiveImage}
+          className={classes.smallSliderButtons}
+        >
+          Click me
+        </button>
+        <div
+          style={{
+            transform: `translateX(-${(elementWidth - 8) * activeImage}px)`,
+          }}
+          className={classes.pictureSliderWrapper}
+        >
+          {images.map((img, index) => (
+            <div
+              key={index}
+              className={classes.pictureSliderImage}
+              ref={elementTarget}
+            >
+              {index}
+            </div>
+          ))}
+        </div>
+        <button onClick={addActiveImage} className={classes.smallSliderButtons}>
+          Click me
+        </button>
       </div>
     </div>
   );
@@ -71,10 +132,7 @@ const PictureSliderBig = ({ size }) => {
   return (
     <div className={classes.bigPictureSLider}>
       <div className={classes.pictureSliderContainer}>
-        <button
-          onClick={subtractActiveImage}
-          className={classes.leftButtonClicker}
-        >
+        <button onClick={subtractActiveImage} className={classes.buttonClicker}>
           Scroll left!
         </button>
         <div
@@ -90,7 +148,7 @@ const PictureSliderBig = ({ size }) => {
             </React.Fragment>
           ))}
         </div>
-        <button onClick={addActiveImage} className={classes.rightButtonClicker}>
+        <button onClick={addActiveImage} className={classes.buttonClicker}>
           Scroll right!
         </button>
       </div>
